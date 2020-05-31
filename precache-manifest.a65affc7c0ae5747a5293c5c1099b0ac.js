3d156fb4b76f71bd858476d87c3e11e1dd6d2205
Carl <carl_taylor1990@hotmail.co.uk>
2020-05-31 15:32:37 +0100

    Updates

---
+++ precache-manifest.a65affc7c0ae5747a5293c5c1099b0ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361c6245db3323e78ffc41198a419c33",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85c5d601997d0fa87ef5",
+    "url": "/monsters-rolodex/static/css/main.22d31ebb.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c5d601997d0fa87ef5",
+    "url": "/monsters-rolodex/static/js/main.35e8259a.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
